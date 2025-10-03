de8d85d0fb6c1c0d820b6dae21de1a7260d59bed
Tim de Groot <timdegroot345@gmail.com>
2025-10-03 23:50:22 +0200

    vibe fixx

--- src/Plugin/Action/CopyEmails.php
+++ src/Plugin/Action/CopyEmails.php
@@ -25,28 +25,17 @@ class CopyEmails extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    if (!$entity) {
-      return;
-    }
-
-    // Only collect email if available.
-    if ($entity->hasField('mail') && !$entity->get('mail')->isEmpty()) {
-      $emails[] = $entity->get('mail')->value;
-      // Immediately output via messenger.
-      $this->messenger()->addMessage($this->t('Collected email: @email', [
-        '@email' => $entity->get('mail')->value,
-      ]));
-    }
-
-    // DO NOT return anything.
+    // DO NOTHING HERE for batch safety. All processing happens in executeMultiple().
+    return;
   }
 
   /**
    * {@inheritdoc}
    */
   public function executeMultiple(array $entities) {
-    // Collect emails and output immediately.
     $emails = [];
+
+    // Collect emails.
     foreach ($entities as $entity) {
       if ($entity->hasField('mail') && !$entity->get('mail')->isEmpty()) {
         $emails[] = $entity->get('mail')->value;
@@ -55,18 +44,21 @@ class CopyEmails extends ViewsBulkOperationsActionBase {
 
     if (!empty($emails)) {
       $email_string = implode(',', $emails);
+
+      // Output message. This is safe in a batch process.
       $this->messenger()->addMessage($this->t('Collected emails: @emails', [
         '@emails' => $email_string,
       ]));
     }
 
-    // DO NOT return anything.
+    // DO NOT return anything!
   }
 
   /**
    * {@inheritdoc}
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    // Only allow admins or users with view access.
     return $account->hasPermission('administer users')
       ? TRUE
       : $object->access('view', $account, $return_as_object);
